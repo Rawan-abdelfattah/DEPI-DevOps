7e3f371cd5a543432448973a6d9c54f8ff8adaf4
JanaElnagar <es-jana.soliman2025@alexu.edu.eg>
2024-10-18 17:37:43 +0300

    add new playbook

--- quick-run.sh
+++ quick-run.sh
@@ -9,6 +9,7 @@ cp mykey.pub ~/.ssh
 # apply and create instances
 terraform init
 terraform apply --auto-approve
+terraform output -json > ../terraform_outputs.json
 cd ..
 
 # run env setup script
@@ -16,5 +17,10 @@ cd ansible
 ./env_script.sh
 
 # run playbooks for docker setup and deploy
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.aws_ec2.yml docker-setup.yml
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.aws_ec2.yml deploy.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.aws_ec2.yml before-docker.yml
+
+# then run jenkins for build
+
+# then these commands manually:
+# ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.aws_ec2.yml docker-setup.yml
+# ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.aws_ec2.yml deploy.yml
